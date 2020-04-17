30b6a5a3e4c0306552b459a18356f3606923e9fd
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-04-17 09:36:37 +0200

    Improve handling for recent offline with multiple clients

--- backend/src/main/scala/com/github/opengrabeso/loctio/Presence.scala
+++ backend/src/main/scala/com/github/opengrabeso/loctio/Presence.scala
@@ -1,10 +1,9 @@
 package com.github.opengrabeso.loctio
 
-import java.time.{ZoneOffset, ZonedDateTime}
+import java.time.{Duration, ZoneOffset, ZonedDateTime}
 
 import Storage._
 import common.model._
-
 import common.FileStore.FullName
 
 object Presence {
@@ -26,13 +25,18 @@ object Presence {
   }
 
   def listUsers: Seq[(String, LocationInfo)] = {
+    val now = ZonedDateTime.now().withZoneSameInstant(ZoneOffset.UTC)
     val items = enumerate("state/")
     items.map(i => i._2 -> load[PresenceInfo](i._1)).flatMap { case (login, data) =>
       // we need to transfer the time as UTC, otherwise the JS client is unable to decode it
       data.map { d =>
         val lastSeen = d.lastSeen.withZoneSameInstant(ZoneOffset.UTC)
+        val age = Duration.between(lastSeen, now).getSeconds
+        // when seen in a last minute, always report as online
+        // when one client has reported going offline, there still may be other clients running
+        val state = if (age < 70) "online" else d.state
         //println(s"Report $login as $d")
-        login -> LocationInfo(Locations.locationFromIpAddress(d.ipAddress), lastSeen, d.state)
+        login -> LocationInfo(Locations.locationFromIpAddress(d.ipAddress), lastSeen, state)
       }
     }
   }.toSeq
