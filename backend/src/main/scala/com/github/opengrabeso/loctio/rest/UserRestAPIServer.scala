8f3df645693daeed9839719eb712527b21d47518
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-05-07 16:26:46 +0200

    Partial conversion to ScalaTags

--- backend/src/main/scala/com/github/opengrabeso/loctio/rest/UserRestAPIServer.scala
+++ backend/src/main/scala/com/github/opengrabeso/loctio/rest/UserRestAPIServer.scala
@@ -488,7 +488,7 @@ class UserRestAPIServer(val userAuth: Main.GitHubAuthResult) extends UserRestAPI
                     val workflowLink = a(
                       href := s"""https://github.com/${n.repository.full_name}/actions?query=${queryParEncode(s"""workflow:"$workflow"""")}""",
                       workflow
-                    ).render.tap(println)
+                    ).render
 
                     val title = s"""<span class="message title">$workflowLink$infix$branch$postfix</span>"""
 
@@ -542,21 +542,23 @@ class UserRestAPIServer(val userAuth: Main.GitHubAuthResult) extends UserRestAPI
             }.getOrElse(header)
           }
 
-          val notificationsTable =
-          //language=HTML
-            s"""<html>
-              <head>
-              <link href="static/tray.css" rel="stylesheet" />
-              <link href="rest/issues.css" rel="stylesheet" />
-              </head>
-              <body class="notifications">
-                <p><a href="https://www.github.com/notifications">GitHub notifications</a></p>
-                <div class="notification table">
-                ${unread.map("<div class='notification item'>" + notificationHTML(_, comments, infos) + "</div>").mkString}
-                </div>
-              </body>
-             </html>
-            """
+          val notificationsTable = html(
+            head(
+              link(href := "static/tray.css", rel := "stylesheet"),
+              link(href := "rest/issues.css", rel := "stylesheet"),
+            ),
+            body(
+              cls := "notifications",
+              p(
+                a(href := "https://www.github.com/notifications", "GitHub notifications")
+              ),
+              div(
+                cls := "notification table",
+                raw(unread.map("<div class='notification item'>" + notificationHTML(_, comments, infos) + "</div>").mkString)
+              )
+
+            )
+          ).render
 
           // is it really sorted by updated_at, or some other (internal) update timestamp (e.g. when last_read_at is higher than updated_at?)
           val mostRecentNotified = recentSession.flatMap(_.mostRecentNotified)
